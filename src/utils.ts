47a76c2db65ebba6d6b26b0386413ba680a49630
Louis <louisandrew3@gmail.com>
2024-01-24 21:47:46 +0100

    feat(resume): update resume content, optimized for portfolio

--- src/utils.ts
+++ src/utils.ts
@@ -3,9 +3,10 @@ export const parseContent = (content: string) => {
   const urlRegex =
     /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/gi;
   const hasUrl = new RegExp(urlRegex).test(content);
+  const contentWithNewLine = content.replaceAll("\n", "<br />");
 
   if (hasUrl) {
-    let output = content;
+    let output = contentWithNewLine;
     const matches = content.matchAll(urlRegex);
     for (const match of matches) {
       const [url] = match;
@@ -16,5 +17,5 @@ export const parseContent = (content: string) => {
     return output;
   }
 
-  return content;
+  return contentWithNewLine;
 };
